c4880ffde0d61ec2b8e29161ad4f97d3a3a4f713
Chenkaow, Kasama (Agoda) <Kasama.Chenkaow@agoda.com>
2017-10-26 10:54:40 +0700

    fix browser was not closed when exception occurred

--- services/black-horse.js
+++ services/black-horse.js
@@ -3,17 +3,23 @@ const await = require('asyncawait/await');
 const puppeteer = require('puppeteer');
 
 const run = async((url) => {
-    const browser = await(puppeteer.launch({ args: ['--no-sandbox','--disable-setuid-sandbox'], ignoreHTTPSErrors: true }));
+    const browser = await(puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], ignoreHTTPSErrors: true }));
     const page = await(browser.newPage());
 
-    //await(page.setUserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'));
-    await(page.setViewport({ width: 1920, height: 1080 }));
-    await(page.goto(`${url}`, { waitUntil: 'networkidle', timeout: 30000 }));
+    try {
+        await(page.setUserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'));
+        await(page.setViewport({ width: 1920, height: 1080 }));
+        await(page.goto(`${url}`, { waitUntil: 'networkidle', timeout: 30000 }));
 
-    var dom = await(page.content());
-    await(page.close());
-    await(browser.close());
-    return dom;
+        var dom = await(page.content());
+        await(page.close());
+        await(browser.close());
+        return dom;
+    } catch(err) {
+        await(page.close());
+        await(browser.close());
+        throw err;
+    }
 });
 
 const ways = async((dom, url) => {
@@ -21,13 +27,13 @@ const ways = async((dom, url) => {
     const raw_links = dom.match(pattern);
     const result = raw_links.reduce((res, link) => {
         let href_url = '';
-        if(/(?:https?|cdn)/i.test(link)){
+        if (/(?:https?|cdn)/i.test(link)) {
             href_url = link;
         } else {
             const href_pattern = /(.*?href=")\/?(.*?)(".*)/i;
-            href_url  = link.replace(href_pattern, '$1' + `${url}/` + '$2$3');
+            href_url = link.replace(href_pattern, '$1' + `${url}/` + '$2$3');
         }
-       
+
         return res + `${href_url}` + '<br />';
     }, '');
 
@@ -39,13 +45,13 @@ const see = async((dom, url) => {
     const raw = dom.match(pattern);
     const result = raw.reduce((res, link) => {
         let href_url = '';
-        if(/(?:https?|cdn)/i.test(link)){
+        if (/(?:https?|cdn)/i.test(link)) {
             href_url = link;
         } else {
             const href_pattern = /(.*?src=")\/?(.*?)(".*)/i;
-            href_url  = link.replace(href_pattern, '$1' + `${url}/` + '$2$3');
+            href_url = link.replace(href_pattern, '$1' + `${url}/` + '$2$3');
         }
-       
+
         return res + `${href_url}` + '<br />';
     }, '');
 
